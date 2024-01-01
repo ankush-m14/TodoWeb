49e67f184ec71545366666bf9a2aaba55daf6823
ankush-m14 <ankushmunde1996@gmail.com>
2024-01-01 17:33:50 +0530

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10001, 'Ankush_m14', 'Get AWS Certified', CURRENT_DATE,false);
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10002, 'Ankush_m14', 'Get Java Certified', CURRENT_DATE,false);
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10003, 'Ankush_m14', 'Get SQL Certified', CURRENT_DATE,false);
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10004, 'Ankush_m14', 'Get Google Cloud Certified', CURRENT_DATE,false);
